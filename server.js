34a3289d7d944403dfd58fcf1ce92aea91634613
navaneethnavi <navaneethnavi@yahoo.co.in>
2018-02-15 10:06:32 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,126 @@
+var http = require('http');
+var querystring = require('querystring');
+const crypto = require('crypto');
+const fs = require('fs');
+var devid='';																						//Device id : Which is send from the board
+var validDeviceId = ['RPI1', 'RPI2', 'RPI3', 'RPI4', 'RPI5', 'RPI6'];								// List of Valid device IDs 
+var dataStore = new  Object();
+
+/*
+fs.writeFile('Valid.txt', '\t\t\t\tAccepted data\n\n'); 
+fs.appendFile('Valid.txt', 'Sl.No.  Dev-ID    Pkt-No:\t\tData\t\t\tHash\n');
+var sl1=0; 
+fs.writeFile('Invalid.txt', '\t\t\t\tInvalid data\n\n'); 
+fs.appendFile('Invalid.txt', 'Sl.No.  Dev-ID    Pkt-No:\t\tData\t\t\tHash\n');
+var sl2=0;
+fs.writeFile('Rouge.txt', '\t\t\t\tRouge Devices\n\n'); 
+fs.appendFile('Rouge.txt', 'Sl.No.  Dev-ID    Pkt-No:\t\tData\t\t\tHash\n');
+var sl3
+*/
+
+http.createServer(function(request, response)
+{
+    
+    request.on('data', function (data)										
+        {			
+            jsonParsed = JSON.parse(data); 															//Json parsed data recieved from the client
+            devid = jsonParsed.DevId; 
+            pktno = jsonParsed.PacketNo;
+            console.log('\nThis data is recieved from Device : '+"'"+devid+"'"+', Recieved Packet Number : '+pktno);       
+            if(validDeviceId.indexOf(jsonParsed.DevId) != -1)										//returns  the index if match is found returns -1 if match is not found
+			{	
+				if(dataStore[devid]== undefined || dataStore[devid].data1=='')
+				{
+					dataStore[devid ]= new Object();												//creates a new object only once when data is rceved fro a new device.			
+					dataStore[devid].data1 = jsonParsed.Sdata+jsonParsed.TimeS+jsonParsed.RSSI;
+					dataStore[devid].dhash1=jsonParsed.SHA256Hash;
+					console.log('\nData-1: '+dataStore[devid].data1);
+					response.writeHead(200, { 'Content-Type': 'text/plain'});
+					response.end('Hello Node.js Server has recived the data');
+				}
+				else
+				{
+					//lastvalidpacket=dataStore[devid].data1;
+					dataStore[devid].data2 = jsonParsed.Sdata+jsonParsed.TimeS+jsonParsed.RSSI;
+					dataStore[devid].dhash2=jsonParsed.SHA256Hash;
+					
+					console.log('\nData1: '+dataStore[devid].data1);
+					console.log('Data2: '+dataStore[devid].data2);
+					
+					dataStore[devid].concdata = dataStore[devid].data1 + dataStore[devid].data2;					
+					
+					dataStore[devid].Servhash=crypto.createHash('sha256').update(dataStore[devid].concdata).digest('hex');
+					
+					console.log('Composed-Data : '+dataStore[devid].concdata);
+					console.log('\nServer Hash : '+dataStore[devid].Servhash);
+					console.log('Client Hash : '+dataStore[devid].dhash1);
+					if(dataStore[devid].Servhash==dataStore[devid].dhash1)
+					{
+						console.log('\n\t\tData Validated');
+						/*
+            fs.appendFile('Valid.txt',sl1+'\t  '+devid+'     |'+pktno+'|\t'+dataStore[devid].data1+'\t'+dataStore[devid].dhash1+'\n' , (err) =>
+						{ 
+							if (err) throw err;
+							sl1+=1;
+						});
+            */
+						dataStore[devid].dhash1=dataStore[devid].dhash2;
+						dataStore[devid].data1=dataStore[devid].data2;
+					}
+					else
+					{
+						/*
+            fs.appendFile('Invalid.txt',sl2+'\t  '+devid+'     |'+pktno+'|\t'+dataStore[devid].data2+'\t'+dataStore[devid].dhash2+'\n' , (err) =>
+						{ 
+							if (err) throw err;
+							sl2+=1;
+						});
+            */
+						console.log('\n\t      ----------------------------------------------------');
+						console.log('\t******Invalid Packet :Block did not get added to the Chain******');
+						console.log('\t      ----------------------------------------------------');
+					}
+					response.setHeader('Content-Type', 'text/html');
+					response.writeHead(200, { 'Content-Type': 'text/plain'});
+					response.end('Server has received the message');
+					
+				/*	response.writeHead(200, { 'Content-Type': 'text/plain','Trailer': 'Server-Message' });
+					response.addTrailers({ 'Server-Message': 'Ok' });
+					response.end();  */
+				}
+			} 					
+			else
+			{
+			/*
+        fs.appendFile('Rouge.txt',sl3+'\t  '+devid+'     |'+pktno+'|\t'+jsonParsed.Sdata+jsonParsed.TimeS+jsonParsed.RSSI+'\t'+jsonParsed.SHA256Hash+'\n' , (err) =>
+				{ 
+					if (err) throw err;
+						sl3+=1;
+				});
+        */
+				console.log('\n\t#####################################################');
+				console.log('\t# Data Recieved  from an Invalid Device ID -> '+ '"'+devid+'"'+' #');
+				console.log('\t#####################################################');		
+				
+				response.setHeader('Content-Type', 'text/html');
+				response.writeHead(200, { 'Content-Type': 'text/plain'});
+				response.end('Invalid Device  ID |Administrator will be notified');
+				
+				
+				/*const body = 'hello world';
+				response.writeHead(200, body ,{'Content-Length': Buffer.byteLength(body),'Content-Type': 'text/plain' });
+
+				
+				var message = 'Invalid Device ID';
+				response.writeHead(400, message, {'content-type' : 'text/plain'});
+				response.end(message);   
+				
+				response.writeHead(400, { 'Content-Type': 'text/plain','Trailer': 'Server-Message' });
+                response.addTrailers({ 'Server-Message': 'Invalid Device ID' });
+				response.end();	*/
+			}
+							 
+        });
+  
+
+}).listen(80);
